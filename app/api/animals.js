f7238a576291312080b7fcbec0e45254b3461cf6
Jaime <jaime.m.mccandless@gmail.com>
2017-06-15 19:57:31 -0700

    Move stub data to json files

--- app/api/animals.js
+++ app/api/animals.js
@@ -1,62 +1,6 @@
 import { get } from './request';
+import stubAnimalsResponse from './stubAnimals.json';
 
-
-const stubAnimalsResponse = {
-    data: [
-        {
-            type: 'animals',
-            id: 1,
-            attributes: {
-                name: 'Lassie',
-                type: 'Dog',
-                status: 'Active',
-            },
-            relationships: {
-                owner: {
-                    data: {
-                        type: 'members',
-                        id: 'jaime',
-                    },
-                },
-            },
-        },
-        {
-            type: 'animals',
-            id: 2,
-            attributes: {
-                name: 'Bessie',
-                type: 'Cow',
-                status: 'Active',
-            },
-            relationships: {
-                owner: {
-                    data: {
-                        type: 'members',
-                        id: 'jason',
-                    },
-                },
-            },
-        },
-    ],
-    included: [
-        {
-            type: 'members',
-            id: 'jaime',
-            attributes: {
-                name: 'Jaime McCandless',
-                status: 'Active',
-            },
-        },
-        {
-            type: 'members',
-            id: 'jason',
-            attributes: {
-                name: 'Jason Curtis',
-                status: 'Active',
-            },
-        }
-    ],
-};
 
 export function getAnimals() {
     // return get('/api/animals');
--- app/api/members.js
+++ app/api/members.js
@@ -1,28 +1,6 @@
+import stubMembersResponse from './stubMembers.json';
 import { get } from './request';
 
-
-const stubMembersResponse = {
-    data: [
-        {
-            type: 'members',
-            id: 'jaime',
-            attributes: {
-                firstName: 'Jaime',
-                lastName: 'McCandless',
-                demNumber: 'SR00218',
-            },
-        },
-        {
-            type: 'members',
-            id: 'jason',
-            attributes: {
-                firstName: 'Jason',
-                lastName: 'Curtis',
-                demNumber: 'SR00213',
-            },
-        }
-    ],
-};
 
 export function getMembers() {
     // return get('/api/members');
